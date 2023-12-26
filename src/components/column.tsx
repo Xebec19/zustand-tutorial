c4e46c8eacf40afc84ee8c7b8c3c67b97f847790
xebec19 <rohandeveloper106@gmail.com>
2023-12-26 18:31:06 +0530

    feat: added layout

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,11 +1,14 @@
+import Task from "./task";
+
 export default function Column({
   status,
 }: {
   status: "planned" | "ongoing" | "done";
 }) {
   return (
-    <div className="min-h-[20rem] p-2 rounded-md bg-cyan-600 max-w-[32vw] text-white">
-      {status}
+    <div className="min-h-[20rem] p-2 rounded-md bg-cyan-600 max-w-[32vw] text-white space-y-2">
+      <p className="px-2 border-b">{status}</p>
+      <Task title="dummy title" />
     </div>
   );
 }
---
+++ src/components/task.tsx
@@ -0,0 +1,3 @@
+export default function Task({ title }: { title: string }) {
+  return <div className="rounded-md bg-cyan-200 p-2 text-black">{title}</div>;
+}
